3fea0f144f0fa1a6537bbaf65d7856fd9b3b9d06
Alexis Naudin <alexis.naudin@gmail.com>
2020-03-26 15:03:40 +0100

    add server.js to repo

---
+++ backend/server.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const neatCsv = require('neat-csv');
+const url = require('url') 
+const path = require('path')
+const app = express()
+const fs = require("fs")
+const req = require("request")
+/* const csv = require('./jquery.csv.js'); */
+
+
+app.get('/data', function (req, res) {
+  fs.readFile('./CleanDB.csv', async (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    res.send(await neatCsv(data))
+  })
+})
+
+app.get('/data2', function (req, res) {
+  fs.readFile('./CleanDB2.csv', async (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    res.send(await neatCsv(data))
+  })
+})
+
+app.get('/*', function(req, res){
+  var filename;
+  var uri = req.originalUrl;
+  filename = path.join(process.cwd(), uri);
+  fs.exists(filename, function (exists) {
+      if (!exists) {
+          res.writeHead(404, {
+          "Content-Type": "text/plain"
+        });
+        res.write("404 Not Found\n");
+        res.end();
+        return;
+      }
+  if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+  fs.readFile(filename, "binary", function (err, file) {
+      if (err) {
+          res.writeHead(500, {
+          "Content-Type": "text/plain"
+        });
+        res.write(err + "\n");
+        res.end();
+        return;
+      }
+      res.writeHead(200);
+      res.write(file, "binary");
+      res.end();
+    });
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
