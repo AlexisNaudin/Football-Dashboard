2ba13c6c029af9e64c2d4c162e0ef82e4b852297
Alexis Naudin <alexis.naudin@gmail.com>
2020-03-31 10:10:08 +0200

    improvment of dashboard layout

--- backend/frontend/myScript.js
+++ backend/frontend/myScript.js
@@ -21,29 +21,27 @@ $.get("/data", function(data, status){
 });
 */
 
+
 var teams_div_season = [];
 $.get("/data", function(data, status){
     
     var Division = [""];
-    var Seasons = [""];
-    var Teams = [""];
-    var elements = [Division, Seasons, Teams]
-    var associated = ["Div", "Season", "HomeTeam"]
-    elements.forEach(function(element, index){
         for(var i=0; i<data.length; i++){
             // If element does not exist, add it to the array
-            if(element.indexOf(data[i][associated[index]]) === -1){
-                element.push(data[i][associated[index]]);
+            if(Division.indexOf(data[i]["Div"]) === -1){
+                Division.push(data[i]["Div"]);
             }
         }
-    });
+    // Similar way of doing it:
+    /*
+    var Division = [];
+    Division = d3.set(data.map(d => d["Div"])).values();
+    Division.unshift("");
+    */
     
     // subset of the data keeping only properties Div, Season and HomeTeam of objects in
     // array "data" that will be used for the dropdown selection.
     var subdata = data
-    const dropdownData = subdata.map(({Div, Season, HomeTeam}) => ({
-        Div, Season, HomeTeam
-    }));
     
     // unique values from object property: const unique = [...new Set(subdata.map(item => item.Season))];
 
@@ -54,18 +52,24 @@ $.get("/data", function(data, status){
         var tempdata = [];
         tempdata = subdata.filter(x => x.Div === element); // subset of data for given division in "Division"
         var seasons = [];
+            // Create an array of unique seasons from data subset. 
+            // Can also be done in one line: seasons = d3.set(tempdata.map(d => d["Season"])).values();
             for(var i=0; i<tempdata.length; i++){
-                // Create an array of unique seasons from data subset
                 if(seasons.indexOf(tempdata[i]["Season"]) === -1){
                     seasons.push(tempdata[i]["Season"]);
                 }
             }
+
         seasons.forEach(function(element2){ // for each season in data subset
-            for(var i=0; i<dropdownData.length; i++){
                 // select given division and given season:
                 tempdata = subdata.filter(x => x.Div === element && x.Season === element2); 
-                teams_season[element2] = [...new Set(tempdata.map(item => item.HomeTeam))];     
-            }
+                teams_season[element2] = [...new Set(tempdata.map(item => item.HomeTeam))];   
+            
+        })
+
+        seasons.forEach(function(element2){
+            teams_season[element2].unshift("Choose a team");  
+        
         })
         // add temorary array "teams_season" containing teams of given division and season 
         // to "teams_div_season" for each division "element":
@@ -78,32 +82,39 @@ $.get("/data", function(data, status){
     // Create data = list of groups
     // var Division = ["E0", "E1", "L1"]
     //Create and append select list
-    var title1 = document.createElement("h4");
+    var title1 = document.createElement("h6");
+    title1.className = "Nav_titles"
     title1.innerHTML = "Championship"
     myParent.appendChild(title1);
     var selectList = document.createElement("select");
     selectList.id = "mylist1";
     selectList.name = "mylist1";
+    selectList.className = "dropdowns";
     selectList.setAttribute("onchange", "populate(this.id, 'mylist2')");
-    myParent.appendChild(selectList);
+    myParent.appendChild(selectList); 
 
-    var title2 = document.createElement("h4");
+    var title2 = document.createElement("h6");
+    title2.className = "Nav_titles"
     title2.innerHTML = "Season"
     myParent.appendChild(title2);
     var selectList2 = document.createElement("select");
     selectList2.id = "mylist2";
     selectList2.name = "mylist2";
+    selectList2.className = "dropdowns";
     selectList2.setAttribute("onchange", "populate2('mylist1', this.id, 'mylist3')");
     myParent.appendChild(selectList2);
     
-    var title3 = document.createElement("h4");
-    title3.innerHTML = "Choose your team"
+    var title3 = document.createElement("h6");
+    title3.className = "Nav_titles"
+    title3.innerHTML = "Team"
     myParent.appendChild(title3);
     var selectList3 = document.createElement("select");
     selectList3.id = "mylist3";
     selectList3.name = "mylist3";
+    selectList3.className = "dropdowns";
     selectList3.setAttribute("onchange", "updateChart('mylist2', this.id)");
     myParent.appendChild(selectList3);
+
 
     //Create and append the options
     for (var i = 0; i < Object.keys(teams_div_season).length; i++) {
@@ -115,7 +126,82 @@ $.get("/data", function(data, status){
 
 });
 
+/*
+// D3 HISTOGRAM
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 40},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
+// append the svg object to the body of the page
+var svg = d3.select("#Chart2")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// get the data
+d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_doubleHist.csv", function(data) {
+
+  // X axis: scale and draw:
+  var x = d3.scaleLinear()
+      .domain([-4,9])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+      .range([0, width]);
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  // set the parameters for the histogram
+  var histogram = d3.histogram()
+      .value(function(d) { return +d.value; })   // I need to give the vector of value
+      .domain(x.domain())  // then the domain of the graphic
+      .thresholds(x.ticks(40)); // then the numbers of bins
+
+  // And apply twice this function to data to get the bins.
+  var bins1 = histogram(data.filter( function(d){return d.type === "variable 1"} ));
+  var bins2 = histogram(data.filter( function(d){return d.type === "variable 2"} ));
+
+  // Y axis: scale and draw:
+  var y = d3.scaleLinear()
+      .range([height, 0]);
+      y.domain([0, d3.max(bins1, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+  svg.append("g")
+      .call(d3.axisLeft(y));
+
+  // append the bars for series 1
+  svg.selectAll("rect")
+      .data(bins1)
+      .enter()
+      .append("rect")
+        .attr("x", 1)
+        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+        .attr("height", function(d) { return height - y(d.length); })
+        .style("fill", "#69b3a2")
+        .style("opacity", 0.6)
+
+  // append the bars for series 2
+  svg.selectAll("rect2")
+      .data(bins2)
+      .enter()
+      .append("rect")
+        .attr("x", 1)
+        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+        .attr("height", function(d) { return height - y(d.length); })
+        .style("fill", "#404080")
+        .style("opacity", 0.6)
+
+  // Handmade legend
+  svg.append("circle").attr("cx",300).attr("cy",30).attr("r", 6).style("fill", "#69b3a2")
+  svg.append("circle").attr("cx",300).attr("cy",60).attr("r", 6).style("fill", "#404080")
+  svg.append("text").attr("x", 320).attr("y", 30).text("variable A").style("font-size", "15px").attr("alignment-baseline","middle")
+  svg.append("text").attr("x", 320).attr("y", 60).text("variable B").style("font-size", "15px").attr("alignment-baseline","middle")
+
+});
+*/
 function populate(l1, l2){
     var l1 = document.getElementById(l1);
     var l2 = document.getElementById(l2);
@@ -186,6 +272,11 @@ function updateChart(mySeason, myTeam) {
         obj.bindto="#Chart1";
         obj.data = {};
         obj.data.columns = [];
+        obj.axis = {};
+        obj.axis.y = {};
+        obj.axis.y.inverted = "true";
+        obj.axis.y.max = teams_div_season[data[0]["Div"]][mySeason].length;
+        obj.axis.y.min = 1;
         var axis = [];
         console.log(data);
         for(var i=0; i<data.length; i++){
@@ -194,6 +285,7 @@ function updateChart(mySeason, myTeam) {
             obj.data.columns[count] = [];
             obj.data.columns[count].push(data[i].HomeTeam);
             obj.data.columns[count].push(data[i].HomeTeamRank);
+
          }else{
             obj.data.columns[count].push(data[i].HomeTeamRank);
          }
@@ -202,19 +294,22 @@ function updateChart(mySeason, myTeam) {
     });
 }
 
-
 function findItem(axis, item){
-    for(i=0; i<axis.length; i++){
-        if(axis[i].id===item){
-            return axis[i].count;
+    if(axis.length>0){
+        for(i=0; i<axis.length; i++){
+            if(axis[i].id===item){
+                return axis[i].count;
+            }
         }
+    }else{
+        axis.push({
+            id: item,
+            count: gCount
+        });
+        gCount++;
+        return gCount-1
     }
-    axis.push({
-        id: item,
-        count: gCount
-    });
-    gCount++;
-    return gCount-1
+    
 }
 
 
