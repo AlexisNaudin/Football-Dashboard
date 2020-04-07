d6320074b8bc49d5d3cbbda6d71dd4c11fcb8872
Alexis Naudin <alexis.naudin@gmail.com>
2020-04-07 20:20:33 +0200

    graph improvements

--- backend/frontend/myScript.js
+++ backend/frontend/myScript.js
@@ -128,82 +128,7 @@ $.get("/data", function(data, status){
 
 });
 
-/*
-// D3 HISTOGRAM
-// set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 40},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
-var svg = d3.select("#Chart2")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-
-// get the data
-d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_doubleHist.csv", function(data) {
-
-  // X axis: scale and draw:
-  var x = d3.scaleLinear()
-      .domain([-4,9])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-      .range([0, width]);
-  svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-
-  // set the parameters for the histogram
-  var histogram = d3.histogram()
-      .value(function(d) { return +d.value; })   // I need to give the vector of value
-      .domain(x.domain())  // then the domain of the graphic
-      .thresholds(x.ticks(40)); // then the numbers of bins
-
-  // And apply twice this function to data to get the bins.
-  var bins1 = histogram(data.filter( function(d){return d.type === "variable 1"} ));
-  var bins2 = histogram(data.filter( function(d){return d.type === "variable 2"} ));
-
-  // Y axis: scale and draw:
-  var y = d3.scaleLinear()
-      .range([height, 0]);
-      y.domain([0, d3.max(bins1, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
-  svg.append("g")
-      .call(d3.axisLeft(y));
-
-  // append the bars for series 1
-  svg.selectAll("rect")
-      .data(bins1)
-      .enter()
-      .append("rect")
-        .attr("x", 1)
-        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
-        .attr("height", function(d) { return height - y(d.length); })
-        .style("fill", "#69b3a2")
-        .style("opacity", 0.6)
-
-  // append the bars for series 2
-  svg.selectAll("rect2")
-      .data(bins2)
-      .enter()
-      .append("rect")
-        .attr("x", 1)
-        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
-        .attr("height", function(d) { return height - y(d.length); })
-        .style("fill", "#404080")
-        .style("opacity", 0.6)
-
-  // Handmade legend
-  svg.append("circle").attr("cx",300).attr("cy",30).attr("r", 6).style("fill", "#69b3a2")
-  svg.append("circle").attr("cx",300).attr("cy",60).attr("r", 6).style("fill", "#404080")
-  svg.append("text").attr("x", 320).attr("y", 30).text("variable A").style("font-size", "15px").attr("alignment-baseline","middle")
-  svg.append("text").attr("x", 320).attr("y", 60).text("variable B").style("font-size", "15px").attr("alignment-baseline","middle")
-
-});
-*/
 function populate(l1, l2){
     var l1 = document.getElementById(l1);
     var l2 = document.getElementById(l2);
@@ -276,14 +201,36 @@ function updateData(mySeason, myTeam) {
     var mySeason = document.getElementById(mySeason).value;
     var myTeam = document.getElementById(myTeam).value;
 
+    // D3 HISTOGRAM
+    const margin = {top: 20, right: 20, bottom: 20, left: 45},
+    width = 550 - margin.left - margin.right,
+    height = 260 - margin.top - margin.bottom;
+    
+    const x = d3.scaleBand()
+    .range([0, width])
+    .padding(0.1);
+    
+    const y = d3.scaleLinear()
+    .range([height, 0]);
+    
+    const svg = d3.select("#Chart2").append("svg")
+    .attr("id", "svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+    const div = d3.select("body").append("div")
+    .attr("class", "tooltip")         
+    .style("opacity", 0);
+
+
     var Team_rank = [];
     var Results = [];
 
     $.get("/data", function(data, status){
-        data = data.filter(x => x.Team === myTeam);
+        data = data.filter(x => x.Team === myTeam && x.Season === mySeason);
         Team_rank = data[data.length-1]["ranking"];
-
-        data = data.filter(x => x.Season === mySeason);
 
         countWins = 0;
         countDraws = 0;
@@ -298,7 +245,62 @@ function updateData(mySeason, myTeam) {
                 countLosses++;
             }
         }
-         
+
+        Shots = 0;
+        countShots = 0;
+        Shots_target = 0;
+        countShots_target = 0,
+        Goals_scored = 0;
+        countGoals_scored = 0;
+        Goals_against = 0;
+        countGoals_against = 0;
+        Yellow_cards = 0;
+        Red_cards = 0;
+        Corners = 0;
+        countCorners = 0;
+        for(var i = 0; i<data.length; i++){
+            if(data[i]["Shots"] == "NA" || Number(data[i]["Shots"]) == NaN){
+            } else {
+                Shots = Shots + Number(data[i]["Shots"]);
+                countShots++;
+            }
+            
+            if(data[i]["Shots_target"] == "NA" || Number(data[i]["Shots_target"]) == NaN){
+            } else {
+                Shots_target = Shots_target + Number(data[i]["Shots_target"]);
+                countShots_target++;
+            }
+
+            if(data[i]["Goals_scored"] == "NA" || Number(data[i]["Goals_scored"]) == NaN){
+            } else {
+                Goals_scored = Goals_scored + Number(data[i]["Goals_scored"]);
+                countGoals_scored++;
+            }
+            
+            if(data[i]["Goals_against"] == "NA" || Number(data[i]["Goals_against"]) == NaN){
+            } else {
+                Goals_against = Goals_against + Number(data[i]["Goals_against"]);
+                countGoals_against++;
+            }
+
+            if(data[i]["Yellow_cards"] == "NA" || Number(data[i]["Yellow_cards"]) == NaN){
+            } else {
+                Yellow_cards = Yellow_cards + Number(data[i]["Yellow_cards"]);
+            }
+
+            if(data[i]["Red_cards"] == "NA" || Number(data[i]["Red_cards"]) == NaN){
+            } else {
+                Red_cards = Red_cards + Number(data[i]["Red_cards"]);
+            }
+            
+            if(data[i]["Corners"] == "NA" || Number(data[i]["Corners"]) == NaN){
+            } else {
+                Corners = Corners + Number(data[i]["Corners"]);
+                countCorners++;
+            }
+                       
+        }
+                 
         gCount = 0
         count = 0
         var obj = {};
@@ -310,6 +312,23 @@ function updateData(mySeason, myTeam) {
         obj.axis.y.inverted = "true";
         obj.axis.y.max = teams_div_season[data[0]["Div"]][mySeason].length;
         obj.axis.y.min = 1;
+        obj.title = {};
+        obj.title.text = "Evolution of " + myTeam + "'s Ranking";
+        obj.transition = {};
+        obj.transition.duration = 2000;
+        obj.oninit = function () {
+            this.main.append('rect')
+                .style('fill', '#181818')
+                .attr('x', 0.5)
+                .attr('y', -0.5)
+                .attr('width', this.width)
+                .attr('height', this.height)
+              .transition().duration(4000)
+                .attr('x', this.width)
+                .attr('width', 0)
+              .remove();
+        } 
+        
         var axis = [];
         console.log(data);
         for(var i=0; i<data.length; i++){
@@ -357,10 +376,153 @@ function updateData(mySeason, myTeam) {
         pct_losses.innerHTML = "(" + Math.round((countLosses/data.length)*1000)/10 + "% )";
         document.getElementById("Losses").appendChild(nb_losses);
         document.getElementById("Losses").appendChild(pct_losses);
+
+        avg_Shots = document.getElementById("Avg_shots_per_game");
+        avg_Shots.innerHTML = Math.round((Shots/countShots)*10)/10;
+        document.getElementById("Avg_shots").appendChild(avg_Shots);
+ 
+        avg_Shots_target = document.getElementById("Avg_shots_target_per_game");
+        avg_Shots_target.innerHTML = Math.round((Shots_target/countShots_target)*10)/10;
+        document.getElementById("Avg_shots_target").appendChild(avg_Shots_target);
+        
+        nbGoals_scored = document.getElementById("nb_Goals_scored");
+        nbGoals_scored.innerHTML = Goals_scored;
+        document.getElementById("Goals_scored").appendChild(nbGoals_scored);
+
+        nbGoals_against = document.getElementById("nb_Goals_conceeded");
+        nbGoals_against.innerHTML = Goals_against;
+        document.getElementById("Goals_conceeded").appendChild(nbGoals_against);
+
+        nbYellow_cards = document.getElementById("nbYellow");
+        nbYellow_cards.innerHTML = Yellow_cards;
+        document.getElementById("Yellow_cards").appendChild(nbYellow_cards);
+
+        nbRed_cards = document.getElementById("nbRed");
+        nbRed_cards.innerHTML = Red_cards;
+        document.getElementById("Red_cards").appendChild(nbRed_cards);
+
+        nbCorners = document.getElementById("Avg_corner_per_game");
+        nbCorners.innerHTML = Math.round((Corners/countCorners)*10)/10;
+        document.getElementById("Corners").appendChild(nbCorners);
+
+        // D3 Histogram
+        // data
+        var Shots_hist = [];
+        var Shots_target_hist = [];
+
+        for(var i=0; i<data.length; i++){
+            obj = {};
+            obj2 = {};
+            obj.Matchday = i;
+            obj2.Matchday = i;
+            obj.shots = data[i]["Shots"];
+            obj2.shots_target = data[i]["Shots_target"];
+            Shots_hist.push(obj);
+            Shots_target_hist.push(obj2);            
+        }
+
+        Shots_hist.forEach(function(d) {
+            d.shots = +d.shots;
+        });
+
+        Shots_target_hist.forEach(function(d) {
+            d.shots_target = +d.shots_target;
+        });
+
+            // Mise en relation du scale avec les données de notre fichier
+            // Pour l'axe X, c'est la liste des pays
+            // Pour l'axe Y, c'est le max des populations
+            x.domain(Shots_hist.map(function(d) { return d.Matchday; }));
+            y.domain([0, d3.max(Shots_hist, function(d) { return d.shots; })]);
+
+            // Ajout de l'axe X au SVG
+            // Déplacement de l'axe horizontal et du futur texte (via la fonction translate) au bas du SVG
+            // Selection des noeuds text, positionnement puis rotation
+            svg.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x).tickSize(0))
+                .selectAll("text")	
+                    .style("text-anchor", "end")
+                    .attr("dx", "-.8em")
+                    .attr("dy", ".15em")
+                    .attr("transform", "rotate(-65)");
+            
+            // Ajout de l'axe Y au SVG avec 6 éléments de légende en utilisant la fonction ticks (sinon D3JS en place autant qu'il peut).
+            svg.append("g")
+                .call(d3.axisLeft(y).ticks(6));
+        
+            // Ajout des bars en utilisant les données de notre fichier data.tsv
+            // La largeur de la barre est déterminée par la fonction x
+            // La hauteur par la fonction y en tenant compte de la population
+            // La gestion des events de la souris pour le popup
+            svg.selectAll(".bar")
+                .data(Shots_hist)
+            .enter().append("rect")
+                .attr("class", "bar")
+                .attr("x", function(d) { return x(d.Matchday); })
+                .attr("width", x.bandwidth())
+                .attr("y", function(d) { return y(0); })
+                .attr("height", function(d) { return height - y(0); })
+                .style("fill", "#69b3a2")
+                .style("opacity", 0.6)					
+                .on("mouseover", function(d) {
+                    div.transition()        
+                        .duration(200)      
+                        .style("opacity", .9);
+                    div.html("Shots : " + d.shots)
+                        .style("left", (d3.event.pageX + 10) + "px")     
+                        .style("top", (d3.event.pageY - 50) + "px");
+                })
+                .on("mouseout", function(d) {
+                    div.transition()
+                        .duration(500)
+                        .style("opacity", 0);
+                })
+                .transition()
+                .duration(800)
+                .attr("y", function(d) { return y(d.shots); })
+                .attr("height", function(d) { return height - y(d.shots); })
+                .delay(function(d,i){console.log(i) ; return(i*100)})
+
+                svg.selectAll(".bar2")
+                .data(Shots_target_hist)
+            .enter().append("rect")
+                .attr("class", "bar")
+                .attr("x", function(d) { return x(d.Matchday); })
+                .attr("width", x.bandwidth())
+                .attr("y", function(d) { return y(0); })
+                .attr("height", function(d) { return height - y(0); })
+                .style("fill", "#404080")
+                .style("opacity", 0.6)					
+                .on("mouseover", function(d) {
+                    div.transition()        
+                        .duration(200)      
+                        .style("opacity", .9);
+                    div.html("Shots on target : " + d.shots_target)
+                        .style("left", (d3.event.pageX + 10) + "px")     
+                        .style("top", (d3.event.pageY - 50) + "px");
+                })
+                .on("mouseout", function(d) {
+                    div.transition()
+                        .duration(500)
+                        .style("opacity", 0);
+                })
+                .transition()
+                .duration(800)
+                .attr("y", function(d) { return y(d.shots_target); })
+                .attr("height", function(d) { return height - y(d.shots_target); })
+                .delay(function(d,i){console.log(i) ; return(i*25)})
+                    
+        /* // Handmade legend
+        svg.append("circle").attr("cx",300).attr("cy",30).attr("r", 6).style("fill", "#69b3a2")
+        svg.append("circle").attr("cx",300).attr("cy",60).attr("r", 6).style("fill", "#404080")
+        svg.append("text").attr("x", 320).attr("y", 30).text("variable A").style("font-size", "15px").attr("alignment-baseline","middle")
+        svg.append("text").attr("x", 320).attr("y", 60).text("variable B").style("font-size", "15px").attr("alignment-baseline","middle")
+        */
     });
     
     Team_header = document.getElementById("Team_Name");
-    Team_header.innerHTML = document.getElementById("mylist3").value;
+    Team_header.innerHTML = document.getElementById("mylist3").value + " - <small>" + document.getElementById("mylist2").value + "</small>";
     document.getElementById("Team_header").appendChild(Team_header);
     
 }
@@ -382,5 +544,4 @@ function findItem(axis, item){
     }
     
 }
-
 
